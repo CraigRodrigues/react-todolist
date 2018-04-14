40a10de811bb2a4b68cdd8b09596a44badd95613
Craig Rodrigues <PagingCraig@gmail.com>
2018-04-14 11:43:33 -0400

    Prettierify

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Todo = ({toggleComplete, deleteTodo, todo, history}) => {
-
-    let completedClass = todo.completed ? 'completed' : '';
-
+const Todo = ({ toggleComplete, deleteTodo, todo, history }) => {
     return (
         <li>
-            <input type='checkbox'
-                onChange={() => toggleComplete(todo.id)}/>
-            <span className={completedClass}
-                onClick={() => history.push(`/edit/${todo.id}`)}>{todo.title}</span>
-            <span role='img'
-                aria-label='delete'
-                onClick={() => deleteTodo(todo.id)}>❌</span>
+            <input type="checkbox" onChange={() => toggleComplete(todo.id)} />
+            <span
+                className={todo.completed ? 'completed' : ''}
+                onClick={() => history.push(`/edit/${todo.id}`)}
+            >
+                {todo.title}
+            </span>
+            <span role="img" aria-label="delete" onClick={() => deleteTodo(todo.id)}>
+                ❌
+            </span>
         </li>
     );
 };
