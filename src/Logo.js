82cb49427303bc663b4621767cd44a87b11b32ce
Craig Rodrigues <PagingCraig@gmail.com>
2018-04-14 11:44:16 -0400

    Prettierify

--- src/Logo.js
+++ src/Logo.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Logo = () => (
-    <h1>TODOS! <span role="img" aria-label="target">🎯</span></h1>
-)
+    <h1>
+        TODOS!{' '}
+        <span role="img" aria-label="target">
+            🎯
+        </span>
+    </h1>
+);
 
 export default Logo;
