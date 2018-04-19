53f4c2fccba32a4a1c3155d8df9ce4fb2da41f3c
Craig Rodrigues <PagingCraig@gmail.com>
2018-04-18 20:19:25 -0400

    Add snapshot for doc updates

--- src/TodosContainer.js
+++ src/TodosContainer.js
@@ -7,42 +7,12 @@ import TodosList from './TodosList';
 import TodoForm from './TodoForm';
 import NotFound from './NotFound';
 
-// [{
-//     id: '1',
-//     complete: false,
-//     title: 'This is a sample Todo',
-//     notes: ''
-// },
-// {
-//     id: '2',
-//     complete: false,
-//     title: 'Click here to EDIT this todo',
-//     notes: ''
-// },
-// {
-//     id: '3',
-//     complete: true,
-//     title: 'This todo is complete! It cannot be edited',
-//     notes: ''
-// },
-// {
-//     id: '4',
-//     complete: false,
-//     title: 'Click any ❌ emoji to delete a todo',
-//     notes: ''
-// },
-// {
-//     id: '5',
-//     complete: false,
-//     title: 'This icon shows that this todo has notes -->',
-//     notes: 'You found the notes! Good job. 👍'
-// }]
-
 export default class TodosContainer extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = { todos: null };
+        this.todosRef = db.collection('todos').doc('craigs-todos');
 
         this.toggleComplete = this.toggleComplete.bind(this);
         this.createTodo = this.createTodo.bind(this);
@@ -52,116 +22,99 @@ export default class TodosContainer extends React.Component {
 
     componentDidMount() {
         this.fetchTodos();
-    }
-
-    fetchTodos() {
-        let myTodos = db.collection('todos').doc('craigs-todos');
-
-        myTodos
-            .get()
-            .then((doc) => {
+        this.todosRef.onSnapshot(
+            (doc) => {
                 if (doc.exists) {
                     this.setState({ todos: doc.data().todos });
                 } else {
                     console.error('No document found');
                 }
-            })
-            .catch((error) => {
-                console.error('Error getting document', error);
-            });
+            },
+            (error) => {
+                console.error('Snapshot Error', error);
+            }
+        );
+    }
+
+    fetchTodos() {
+        this.todosRef.get().catch((error) => console.error('Error getting document', error));
+    }
+
+    updateTodos(todos) {
+        this.todosRef
+            .update({ todos })
+            .catch((error) => console.error('Error updating document', error));
     }
 
     toggleComplete(id) {
-        this.setState({ loading: true }, () => {
-            let selectedTodo = this.state.todos.find((todo) => todo.id === id);
-            let myTodos = db.collection('todos').doc('craigs-todos');
+        let todosCopy = clonedeep(this.state.todos);
+        let selectedTodo = todosCopy.find((todo) => todo.id === id);
 
-            selectedTodo.complete = !selectedTodo.complete;
-            myTodos.update({ todos: this.state.todos }).then(() => {
-                console.log('Document successfully updated!');
-                this.setState({ todos: this.state.todos });
-            });
-        });
+        selectedTodo.complete = !selectedTodo.complete;
+        this.updateTodos(todosCopy);
     }
 
     createTodo(todo) {
-        this.setState({ loading: true }, () => {
-            let todosCopy = clonedeep(this.state.todos);
-            let myTodos = db.collection('todos').doc('craigs-todos');
+        let todosCopy = clonedeep(this.state.todos);
 
-            todosCopy.push(todo);
-            myTodos.update({ todos: todosCopy }).then(() => {
-                console.log('Document successfully updated!');
-                this.setState({ todos: todosCopy });
-            });
-        });
+        todosCopy.push(todo);
+        this.updateTodos(todosCopy);
     }
 
     editTodo(todo) {
-        this.setState({ loading: true }, () => {
-            let todosCopy = clonedeep(this.state.todos);
-            let index = todosCopy.findIndex((x) => x.id === todo.id);
-            let myTodos = db.collection('todos').doc('craigs-todos');
+        let todosCopy = clonedeep(this.state.todos);
+        let index = todosCopy.findIndex((x) => x.id === todo.id);
 
-            todosCopy[index] = todo;
-            myTodos.update({ todos: todosCopy }).then(() => {
-                console.log('Document successfully updated!');
-                this.setState({ todos: todosCopy });
-            });
-        });
+        todosCopy[index] = todo;
+        this.updateTodos(todosCopy);
     }
 
     deleteTodo(id) {
-        this.setState({ loading: true }, () => {
-            let newTodos = this.state.todos.filter((todo) => todo.id !== id);
-            let myTodos = db.collection('todos').doc('craigs-todos');
+        let todosCopy = clonedeep(this.state.todos);
+        let newTodos = todosCopy.filter((todo) => todo.id !== id);
 
-            myTodos.update({ todos: newTodos }).then(() => {
-                console.log('Document successfully updated!');
-                this.setState({ todos: newTodos });
-            });
-        });
+        this.updateTodos(newTodos);
     }
 
     render() {
         return (
             <React.Fragment>
                 <Switch>
-                <Route
-                    exact
-                    path="/"
-                    render={(props) => (
-                        <TodosList
-                            {...props}
-                            todos={this.state.todos}
-                            toggleComplete={this.toggleComplete}
-                            deleteTodo={this.deleteTodo}
-                        />
-                    )}
-                />
-                <Route
-                    path="/new"
-                    render={(props) => (
-                        <TodoForm
-                            {...props}
-                            todos={this.state.todos}
-                            callback={this.createTodo}
-                            action="Create New Todo"
-                        />
-                    )}
-                />
-                <Route
-                    path="/edit/:id"
-                    render={(props) => (
-                        <TodoForm
-                            {...props}
-                            todos={this.state.todos}
-                            callback={this.editTodo}
-                            action="Edit Todo"
-                        />
-                    )}
-                />
-                <Route component={NotFound} />
+                    <Route
+                        exact
+                        path="/"
+                        render={(props) => (
+                            <TodosList
+                                {...props}
+                                todos={this.state.todos}
+                                toggleComplete={this.toggleComplete}
+                                deleteTodo={this.deleteTodo}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/new"
+                        render={(props) => (
+                            <TodoForm
+                                {...props}
+                                todos={this.state.todos}
+                                callback={this.createTodo}
+                                action="Create New Todo"
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/edit/:id"
+                        render={(props) => (
+                            <TodoForm
+                                {...props}
+                                todos={this.state.todos}
+                                callback={this.editTodo}
+                                action="Edit Todo"
+                            />
+                        )}
+                    />
+                    <Route component={NotFound} />
                 </Switch>
             </React.Fragment>
         );
